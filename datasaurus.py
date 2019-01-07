6363c556684c633ce1e18dbebd4ee6f9f20649dd
Tom G <griggles@Toms-MacBook-Pro.local>
2019-01-07 16:58:16 -0500

    begin feature engineering

--- datasaurus.py
+++ datasaurus.py
@@ -35,19 +35,46 @@ def sample_data(funny_threshold=50):
     
     return funnies.append(duds)
     
-wordnet_lemmatizer = WordNetLemmatizer()
 pattern = "([a-zA-Z]+(?:'[a-z]+)?)"
-english = set(nltk.corpus.words.words())
+english_words = set(nltk.corpus.words.words())
 stop_words = set(stopwords.words('english'))
 
-  
-def lemmatize_joke(joke):
+#return cleaned tokens
+def clean_joke(joke, english):
    raw_tokens = nltk.regexp_tokenize(joke, pattern)
    tokens = [t.lower() for t in raw_tokens]
    listed = [t for t in tokens if not t in stop_words]
-   lemmatized = [wordnet_lemmatizer.lemmatize(word, pos="v") for word in listed]
-   #words = list(filter(lambda w: w in english, lemmatized))
-   return " ".join(lemmatized)
+   if english:
+       listed = list(filter(lambda w: w in english_words, listed))
+   return listed
 
-def transform_data(df):
-    df['joke'] = df.apply(lambda row: lemmatize_joke(row['title'] + ' ' + row['body']), axis=1)
+#return stemmed tokens
+stemmer = SnowballStemmer("english")
+def stem_joke(joke, english):
+    listed = clean_joke(joke, english)
+    stemmed = [stemmer.stem(word) for word in listed]
+    return stemmed
+
+#return lemmed tokens
+wordnet_lemmatizer = WordNetLemmatizer()
+def lemmatize_joke(joke, english):
+    listed = clean_joke(joke, english)
+    lemmatized = [wordnet_lemmatizer.lemmatize(word, pos="v") for word in listed]
+    return lemmatized
+
+def untokenize(tokens):
+    return ' '.join(tokens)
+
+#stem = none, lem or stem depending on the type of stemmization we want to do on the words
+#english boolean-value telling us whether we want to filter for english words prior to stemmization
+#rough values: 35,000 unq for ('lem', True)-  16,000 for ('lem', False)
+    #29,000 for ('stem', True) - #9,000 got ('stem', False)
+def transform_data(df, stem='stem', english=True):
+    if (stem == 'lem'):
+        df['joke'] = df.apply(lambda row: untokenize(lemmatize_joke(row['title'] + ' ' + row['body'], english)), axis=1)
+    elif (stem == 'none'):
+        df['joke'] = df.apply(lambda row: untokenize(clean_joke(row['title'] + ' ' + row['body'], english)), axis=1)
+    else:
+        df['joke'] = df.apply(lambda row: untokenize(stem_joke(row['title'] + ' ' + row['body'], english)), axis=1)
+
+    return df
